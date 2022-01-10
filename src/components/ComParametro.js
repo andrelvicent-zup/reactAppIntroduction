05f1385cd7b60e4be13f5b670cb31e7b77be513a
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-10 07:19:43 -0300

    componente com props

---
+++ src/components/ComParametro.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function ComParametro(props) {
+    return (
+        <div>
+            <h1> {props.titulo}</h1>
+            <p>  {props.subtitulo} </p>
+        </div>
+    )
+}
+
+export default ComParametro
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
 
 ReactDOM.render(
-  <Primeiro />,
+<div>
+  <Primeiro />
+  <ComParametro titulo="Componente" subtitulo="create component"/>
+</div>,
   document.getElementById('root')
 );
