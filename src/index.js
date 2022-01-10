cd6387eacf49b89c6b500fadc15e58adbe709e19
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-10 07:25:36 -0300

    componente com filhos

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
 <div>
-  <Primeiro />
-  <ComParametro titulo="Componente" subtitulo="create component"/>
+  <ComFilhos>
+    <ul>
+      <li>Ana</li>
+      <li>Andressa</li>
+      <li>André</li>
+      <li>Abobra</li>
+    </ul>
+  </ComFilhos>
+{  //<Primeiro />
+  //<ComParametro titulo="Componente" subtitulo="create component"/>
+}
 </div>,
   document.getElementById('root')
 );
